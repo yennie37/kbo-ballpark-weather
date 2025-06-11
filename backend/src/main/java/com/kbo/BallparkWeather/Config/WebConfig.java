b08ccaf0ec499bfbe42ad586ec44614893d504c2
yennie37 <yennie37@naver.com>
2025-06-11 13:47:08 +0900

    modify webconfig

--- backend/src/main/java/com/kbo/BallparkWeather/Config/WebConfig.java
+++ backend/src/main/java/com/kbo/BallparkWeather/Config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://kbo-ballpark-weather.vercel.app") // ✅ 프론트 도메인 명시
                 .allowedMethods("*");
     }
 }
